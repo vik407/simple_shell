8099b2ccd9b1e4738c57a68e43265a3dae828914
Victor Hernandez <vik407@gmail.com>
2019-11-18 17:27:39 -0500

    TODO - Simple shell buffer size

--- shell/prompt.c
+++ shell/prompt.c
@@ -8,26 +8,40 @@
 int main(void)
 {
 	size_t bufsize = 1024;
+	/* Contar de mi buff la cantidad de separadores que tengo para hacer este dinámico */
 	char *buff = NULL;
-	int getline_status;
+	int getline_status, i, k;
 	char *token;
+	/* Hacer el malloc luego del cuento de separadores */
+	char **tokens = (char**) malloc(bufsize * sizeof(char *));
 	const char s[2] = " ";
 	int status = 1;
+	i = 0;
 
 	while (status == 1)
 	{
 		printf("$ ");
 		getline_status = getline(&buff, &bufsize, stdin);
 		buff[getline_status - 1] = '\0';
-		printf("%s\n", buff);
+		/* Crear la asignación de memoria
+		* char **tokens = (char**) malloc(bufsize * sizeof(char *));
+		*/
+		printf("Buff: %s\n", buff);
 		token = strtok(buff, s);
 		if (getline_status > 1)
 		{
 			while (token != NULL)
 			{
-				printf( "%s\n", token );
+				tokens[i] = token;
+				printf( "Token: %s\n", token );
 				token = strtok(NULL, s);
+				i++;
 			}
+			for (k = 0; tokens != NULL; k++)
+			{
+				printf( "Tokens[%d]: %s\n", k, tokens[k]);
+			}
+			/* Free malloc tokens */
 		}
 		else if (getline_status == -1)
 		{
